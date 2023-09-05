8c9afc40dfca389741ea952003fc8fb4ac12244f
Ateokyn <ateokyn@gmail.com>
2023-09-05 17:39:37 -0600

    Inicio del proyecto VFS.

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '@kekodroid',
+    database: 'vfs'
+
+});
+
+db.connect((err) =>{
+    if(err){
+        console.error('Error de conexion a la base de datos', err);
+    }else{
+        console.error('Conexion exitosa a la base de datos');
+    }
+});
+
+app.use(cors());
+
+app.listen(port,()=>{
+    console.log(`Servidor backend en funcionamiento en el puerto ${port}`);
+
+});
